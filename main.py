dce0dc7c2a52c74f6287da7e38df1f6dcf4d887f
ubuntu vm <danteantu5@gmail.com>
2023-09-15 00:03:55 -0300

    added parser for orion bms

--- main.py
+++ main.py
@@ -6,6 +6,32 @@ from can import Bus
 from can.message import Message
 from tools.operations import linear_map, voltage_transform
 
+def parsed_message_orion(msg: can.Message):
+    id = msg.arbitration_id
+    data = msg.data
+    parsed_message = {}
+    if id == 0x100:
+        parsed_message["pack_soc"] = data[0]
+        parsed_message["pack_current"] = data[1:3]
+        parsed_message["pack_inst_voltage"] = data[3:5]
+        parsed_message["pack_open_voltage"] = data[5:7]
+        parsed_message["crc_checksum"] = data[7]
+    elif id == 0x101:
+        parsed_message["pack_abs_current"] = data[0:2]
+        parsed_message["max_voltage"] = data[2:4]
+        parsed_message["min_voltage"] = data[4:6]
+        parsed_message["crc_checksum"] = data[6]
+    elif id == 0x102:
+        parsed_message["max_temp"] = data[0]
+        parsed_message["id_max_temp"] = data[1]
+        parsed_message["min_temp"] = data[2]
+        parsed_message["id_min_temp"] = data[3]
+        parsed_message["mean_temp"] = data[4]
+        parsed_message["internal_temp"] = data[5]
+        parsed_message["id_max_volt"] = data[6]
+        parsed_message["id_min_volt"] = data[7]
+        
+
 # TODO: implement this function
 def parsed_message(msg: can.Message):
     id = msg.arbitration_id
