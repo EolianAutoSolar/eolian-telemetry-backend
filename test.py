f4b85f2e3aeda2da6910e0b3a9bdd1f148bdaf3d
Dante Mardones <danteantu5@gmail.com>
2024-01-23 02:13:37 -0300

    working prototype architechture

---
+++ test.py
@@ -0,0 +1,47 @@
+from multiprocessing import Process, Manager
+import time
+import random
+
+class Reader():
+    def __init__(self) -> None:
+        self.d = {}
+
+    def read(self, dc):
+        while True:
+            self.d["C"] = random.randint(0, 10)
+            print("generated data ", self.d)
+            Process(target=dc.update, args=(self.d, )).start()
+            time.sleep(1)
+
+class Data():
+    def __init__(self, services) -> None:
+        m = Manager()
+        self.d = m.dict()
+        self.services = services
+    
+    def update(self, data):
+        for name, value in data.items():
+            self.d[name] = value
+        for s in self.services:
+            Process(target=s.update, args=(self.d, )).start()
+        
+
+class Service():
+    def __init__(self) -> None:
+        m = Manager()
+        self.d = m.dict()
+    
+    def update(self, data):
+        for name, value in data.items():
+            self.d[name] = value
+        print("updated data ", self.d)
+
+r = Reader()
+s = Service()
+dc = Data([s])
+Process(target=r.read, args=(dc, )).start()
+
+while True:
+    print("Service data {}".format(s.d))
+    time.sleep(0.3)
+
