a84f0b38138ad146e4c14b2e252bdf969e5486e1
ElVichoSiu <vthielemunoz@gmail.com>
2024-01-27 15:54:20 -0300

    Develop kelly_extraxtor.py

--- extractor/kelly_extractor.py
+++ extractor/kelly_extractor.py
@@ -1,18 +1,30 @@
 import csv
 
-def readCan0(data, timestamp, msg_id, data):
+def readCan0(data, timestamp, msg_id):
     pass
 
-with open('can0.csv') as csvfile:
+with open('vcan0.csv', 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter = ',', quotechar = '"')
-    i = 0
+    i = -1
     for row in reader:
         i += 1
-        if(i == 1):
+        if(i == 0):
             continue
+        
+        msgs = []
+        datas = []
         
         timestamp = row[0]
         
         msg_id = row[1]
         
+        msgs.append(msg_id)
+        
         data = row[2]
+        
+        datas.append(data)
+        
+        if(msg_id % 10 == 5):
+            c = 1
+            while(int(msgs[i - c] / 100) != int(msg_id / 100)):
+                pass
