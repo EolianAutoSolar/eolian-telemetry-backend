5d6ad6fcfca150a4e9cb4f3cb0a0b1ccb6554256
Dante Mardones <danteantu5@gmail.com>
2024-04-09 20:17:03 -0400

    FEAT: working configuration for real time remote telemetry

--- remote.py
+++ remote.py
@@ -1,8 +1,7 @@
-from telemetry_core import Process, Producer
+from telemetry_core import Process
 from digi.xbee.devices import XBeeDevice
 
-BAUD_RATE = 9600
-
+BAUD_RATE = 230400
 # TODO: Check where we could call the xbee.close() method
 #       if .open() takes little time one could open, send and then close
 #       the device. Otherwise we need a place to call the .close() method
@@ -12,22 +11,26 @@ class RemoteSender(Process):
     def __init__(self, port):
         # TODO: Save xbee
         self.xbee = XBeeDevice(port, BAUD_RATE)
+        self.xbee.open()
         self.counter = 0
+        self.val = 0
         self.data_buffer = ''
 
     def use_data(self, data):
         self.data_buffer += '|' + data['raw_message']
         self.counter += 1
+        self.val +=1
         # TODO: This value is important, it should not be set as a magic number
-        if self.counter == 15:            
-            self.xbee.open()
+        if self.counter == 6:            
             # print("Sent message {}".format(self.data_buffer))
-            self.xbee.send_data_broadcast(self.data_buffer)
-            self.xbee.close()
+            xbee_network = self.xbee.get_network()
+            remote_device = xbee_network.discover_device("XBEE_B")
+            self.xbee.send_data_async(remote_device, self.data_buffer)
+            # self.xbee.close()
             self.counter = 0
             self.data_buffer = ''
     
-class RemoteReceiver(Producer):
+class RemoteReceiver(Process):
 
     def __init__(self, port):
         self.xbee = XBeeDevice(port, BAUD_RATE)
