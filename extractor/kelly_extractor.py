5433ef910e9189779cbdf865bb67e0e90bbb2b5c
ElVichoSiu <vthielemunoz@gmail.com>
2024-04-12 17:27:49 -0400

    FEATURE: Se añade el funcionamiento para query = 0x1b

--- extractor/kelly_extractor.py
+++ extractor/kelly_extractor.py
@@ -1,5 +1,15 @@
 import csv
 
+from tools.operations import linear_map, voltage_transform
+
+messages = {}
+
+def msg_to_data(msg):
+    data = []
+    for i in range(len(msg)//2):
+        data.append(int(msg[i:i+2], 16))
+    return data
+
 with open('vcan0.csv', 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter = ',', quotechar = '"')
     i = 0
@@ -18,8 +28,14 @@ with open('vcan0.csv', 'r') as csvfile:
         
         idkelly = msg_id & 0b111
         
+        datas = msg_to_data(data)
+        
         if query == 0x1b:
-            pass
+            messages["brake"] = linear_map(datas[0], 0, 255, 0, 5)
+            messages["tps"] = linear_map(datas[1], 0, 255, 0, 5)
+            messages["operation_voltage"] = voltage_transform(datas[2])
+            messages["vs"] = linear_map(datas[3],120, 134, 4.75, 5.25)
+            messages["bplus"] = voltage_transform(datas[4])
         elif query == 0x1a:
             pass
         elif query == 0x33:
