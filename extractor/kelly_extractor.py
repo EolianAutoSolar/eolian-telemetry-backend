1e8842b960c97cf0f32fa5fd13b44ec3bbc70634
ElVichoSiu <vthielemunoz@gmail.com>
2024-04-12 17:35:29 -0400

    FEATURE: Se añade el funcionamiento de las demas querys

--- extractor/kelly_extractor.py
+++ extractor/kelly_extractor.py
@@ -37,17 +37,29 @@ with open('vcan0.csv', 'r') as csvfile:
             messages["vs"] = linear_map(datas[3],120, 134, 4.75, 5.25)
             messages["bplus"] = voltage_transform(datas[4])
         elif query == 0x1a:
-            pass
+            messages["Ia"] = datas[0]
+            messages["Ib"] = datas[1]
+            messages["Ic"] = datas[2]
+            messages["Va"] = voltage_transform(datas[3])
+            messages["Vb"] = voltage_transform(datas[4])
+            messages["Vc"] = voltage_transform(datas[5])
         elif query == 0x33:
-            pass
+            messages["pwm"] = datas[0]
+            messages["enable_motor_rotation"] = datas[1]
+            messages["motor_temperature"] = datas[2]
+            messages["controller_temperature"] = datas[3]
+            messages["high_side_heat_sink"] = datas[4]
+            messages["low_side_heat_sink"] = datas[5]
         elif query == 0x37:
-            pass
+            messages["mechanical_speed"] = datas[0]<<8 | datas[1]
+            messages["current_controller"]  = datas[2]
+            messages["error_mechanical_speed"]  = datas[3] <<8 | datas[4]
         elif query == 0x42:
-            pass
+            messages["throttle_switch"] = datas[0]
         elif query == 0x43:
-            pass
+            messages["brake_switch"]  = datas[0]
         elif query == 0x44:
-            pass
+            messages["reverse_switch"]  = datas[0]
         
         if idkelly == 0x000:
             pass
