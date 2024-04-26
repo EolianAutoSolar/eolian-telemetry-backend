676fb9cd9d72071afcadb781f6257f6736e261c2
ElVichoSiu <vthielemunoz@gmail.com>
2024-04-26 17:03:09 -0400

    Add final features to kelly_extractor.py

--- extractor/kelly_extractor.py
+++ extractor/kelly_extractor.py
@@ -2,8 +2,6 @@ import csv
 
 from tools.operations import linear_map, voltage_transform
 
-messages = {}
-
 def msg_to_data(msg):
     data = []
     for i in range(len(msg)//2):
@@ -17,6 +15,11 @@ with open('vcan0.csv', 'r') as csvfile:
         i += 1
         if(i == 1):
             continue
+            
+        messages = []
+        
+        for _ in range(25):
+            messages.append("N")
         
         timestamp = row[0]
         
@@ -30,38 +33,41 @@ with open('vcan0.csv', 'r') as csvfile:
         
         datas = msg_to_data(data)
         
-        if query == 0x1b:
-            messages["brake"] = linear_map(datas[0], 0, 255, 0, 5)
-            messages["tps"] = linear_map(datas[1], 0, 255, 0, 5)
-            messages["operation_voltage"] = voltage_transform(datas[2])
-            messages["vs"] = linear_map(datas[3],120, 134, 4.75, 5.25)
-            messages["bplus"] = voltage_transform(datas[4])
-        elif query == 0x1a:
-            messages["Ia"] = datas[0]
-            messages["Ib"] = datas[1]
-            messages["Ic"] = datas[2]
-            messages["Va"] = voltage_transform(datas[3])
-            messages["Vb"] = voltage_transform(datas[4])
-            messages["Vc"] = voltage_transform(datas[5])
-        elif query == 0x33:
-            messages["pwm"] = datas[0]
-            messages["enable_motor_rotation"] = datas[1]
-            messages["motor_temperature"] = datas[2]
-            messages["controller_temperature"] = datas[3]
-            messages["high_side_heat_sink"] = datas[4]
-            messages["low_side_heat_sink"] = datas[5]
-        elif query == 0x37:
-            messages["mechanical_speed"] = datas[0]<<8 | datas[1]
-            messages["current_controller"]  = datas[2]
-            messages["error_mechanical_speed"]  = datas[3] <<8 | datas[4]
-        elif query == 0x42:
-            messages["throttle_switch"] = datas[0]
-        elif query == 0x43:
-            messages["brake_switch"]  = datas[0]
-        elif query == 0x44:
-            messages["reverse_switch"]  = datas[0]
+        messages[0] = timestamp
+        messages[1] = idkelly
         
-        if idkelly == 0x000:
-            pass
-        elif idkelly == 0x001:
-            pass
+        if query == 0x1b:
+            messages[2] = linear_map(datas[0], 0, 255, 0, 5)
+            messages[3] = linear_map(datas[1], 0, 255, 0, 5)
+            messages[4] = voltage_transform(datas[2])
+            messages[5] = linear_map(datas[3],120, 134, 4.75, 5.25)
+            messages[6] = voltage_transform(datas[4])
+        elif query == 0x1a:
+            messages[7] = datas[0]
+            messages[8] = datas[1]
+            messages[9] = datas[2]
+            messages[10] = voltage_transform(datas[3])
+            messages[11] = voltage_transform(datas[4])
+            messages[12] = voltage_transform(datas[5])
+        elif query == 0x33:
+            messages[13] = datas[0]
+            messages[14] = datas[1]
+            messages[15] = datas[2]
+            messages[16] = datas[3]
+            messages[17] = datas[4]
+            messages[18] = datas[5]
+        elif query == 0x37:
+            messages[19] = datas[0]<<8 | datas[1]
+            messages[20]  = datas[2]
+            messages[21]  = datas[3] <<8 | datas[4]
+        elif query == 0x42:
+            messages[22] = datas[0]
+        elif query == 0x43:
+            messages[23]  = datas[0]
+        elif query == 0x44:
+            messages[24]  = datas[0]
+            
+        with open('kelly.csv', 'a', newline='') as csvfile2:
+            writer = csv.writer(csvfile2)
+            
+            writer.writerow(messages)
