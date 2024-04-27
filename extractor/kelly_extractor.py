0a5062c48e6ec41a0da82a5190a5749469d3133d
ElVichoSiu <vthielemunoz@gmail.com>
2024-04-27 17:26:40 -0400

    Changed the way in wich the functions linear_map and voltage_transform are imported in kelly_extractor.py

--- extractor/kelly_extractor.py
+++ extractor/kelly_extractor.py
@@ -1,6 +1,11 @@
 import csv
 
-from tools.operations import linear_map, voltage_transform
+def linear_map(value: int, min: int, max:int, tomin:int, tomax:int):
+    m = (tomax-tomin)/(max-min)
+    return m*(value-min)+tomin
+
+def voltage_transform(value: int):
+    return value/1.84
 
 def msg_to_data(msg):
     data = []
