2ce59da21e11e80bd4e686bef25128e5e12fabaa
ElVichoSiu <vthielemunoz@gmail.com>
2024-04-28 19:26:39 -0400

    FIXED: Changed paths inside the fucntion check_file to help the search of those files

--- test/process_csv_test.py
+++ test/process_csv_test.py
@@ -1,5 +1,6 @@
 # TEST: Correr este archivo con `python3 process_csv_test.py` y deberia dar successful para los 2 chequeos
 import filecmp
+import os
 
 def process_csv(can_csv) -> None:
     # TOOD: Procesar can.csv y guardarlo en los archivos bms.csv and kelly.csv siguiendo las formulas de traduccion de
@@ -10,7 +11,7 @@ def process_csv(can_csv) -> None:
 
 def check_file(filename):
     print("Checking "+filename+"...")
-    if filecmp.cmp(filename, "expected_"+filename):
+    if filecmp.cmp("extractor\\" + filename, "test\\" + "expected_"+filename):
         print("Test successful!")
     else:
         print("Test unsuccessful: "+filename+" and expected_"+filename+" are not the same")
