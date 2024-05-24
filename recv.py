34199720ef98f04f92f2e931fa35b716ac3a4bc6
Jayki-ZX <danteantu5@gmail.com>
2024-05-24 16:56:41 -0400

    final recv

--- recv.py
+++ recv.py
@@ -11,14 +11,16 @@
 # WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
-from digi.xbee.devices import XBeeDevice
+from digi.xbee.models.address import XBee64BitAddress
+from digi.xbee.devices import XBeeDevice, RemoteXBeeDevice
 
 # TODO: Replace with the serial port where your local module is connected to.
 PORT = "COM3"
 # TODO: Replace with the baud rate of your local module.
 BAUD_RATE = 230400
 
+def p(packet):
+    print(packet)
 
 def main():
     print(" +-------------------------------------------------+")
@@ -32,13 +34,18 @@ def main():
 
         device.flush_queues()
 
+        # device.add_packet_received_callback(p)
+
         print("Waiting for data...\n")
 
         while True:
-            xbee_message = device.read_data()
+            remote = RemoteXBeeDevice(device, 
+                    x64bit_addr=XBee64BitAddress(bytearray.fromhex('13A20041AE890D')))
+            remote.set_sync_ops_timeout(0)
+            xbee_message = device.read_data_from(remote)
             if xbee_message is not None:
                 print("From %s >> %s" % (xbee_message.remote_device.get_64bit_addr(),
-                                         xbee_message.data.decode()))
+                                         xbee_message.data), flush=True)
 
     finally:
         if device is not None and device.is_open():
