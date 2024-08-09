41c14eff1f17a85c4c5c3d5cc9802b2b7b59e6f1
Jayki-ZX <danteantu5@gmail.com>
2024-08-08 22:26:44 -0400

    FEAT: adapted canbus and database to arquitecture

--- database.py
+++ database.py
@@ -3,21 +3,12 @@ from telemetry_core import Process
 
 class Database(Process):
     def __init__(self, file_name: str) -> None:
-        self.data_buffer = ''
-        self.counter = 0
         self.file_name = file_name
-        f = open(file=self.file_name, mode='w')
+        f = open(file=self.file_name, mode='w+')
         f.write("timestamp,msg_id,data\n")
-        f.close()
+        self.f = f
 
     def use_data(self, data) -> None:
-        self.data_buffer += data['raw_message'] + '\n'
-        self.counter += 1
-        if self.counter == 1:
-            # print("saved data")
-            f = open(file=self.file_name, mode='a')
-            # f.write("{},{},{}\n".format(msg.timestamp, msg.arbitration_id, msg.data.hex()))
-            f.write(self.data_buffer)
-            f.close()
-            self.data_buffer = ''
-            self.counter = 0
+        # f.write("{},{},{}\n".format(msg.timestamp, msg.arbitration_id, msg.data.hex()))
+        self.f.write("{},{},{}\n".format(data.timestamp, data.arbitration_id, data.data.hex()))
+        self.f.flush()
--- thtestos.py
+++ thtestos.py
@@ -1,49 +1,52 @@
 import threading
 import time
 import queue
-
-###
-### PRODUCTOR (maxima prioridad)    CONSUMIDOR (--)
-### -- Mientras productor espera un mensaje -> Los consumidores se esten ejecutando
-### -- Mientras no hayan mensajes leidos ni procesados, ambos procesos esperan
-### + Asumiendo que el tiempo entre mensajes alcanza para que todos los consumidores se ejecuten
-###
+import can
+from database import Database
+from datetime import datetime
 
 q = queue.Queue()
-
-
-def read_data():
-    return input("Ingrese un mensaje: \n")
+can_reader = can.Bus(interface='virtual', channel='vcan0', receive_own_messages=True)
+messages = 0
 
 def producer(qs):
     while True:
-        dict = read_data()
-        for q in qs:
-            q.put(dict)
+        try:
+            data = can_reader.recv()
+            for q in qs:
+                q.put(data)
+        except KeyboardInterrupt:
+            exit()
 
-## - Considerando el mensaje como el estado global
-## 1. Definir read data, que no tenga busy waiting -> [Producer] vicho
-## 2. Scheduling y cola
-##    - Definir 1 consumers -> dante
-##    - Logica de scheduler para consumers: Cola -> ambos
-# viernes
+db = Database("mttest.txt")
 
-def use_data(data):
-    print("Used data {}".format(data))
-
-# TODO: comparar opciones:
-# cada consumnameor accede a la queue
-# el main process maneja la queue y le pasa un objeto a los consumnameores
 def consumer(q, name):
+    global messages
     while True:
         data = q.get()
-        print("C{} got data {}".format(name, data))
+        db.use_data(data)
+        messages += 1
 
 if __name__ == "__main__":
-    n = 3
+    n = 1
     consumer_queues = [queue.Queue(1) for i in range(n)]
 
+    # save threads to not be deleted by python gc
+    consumer_threads = []
     for i in range(n):
-        threading.Thread(target=consumer, args=(consumer_queues[i], i)).start()
+        t = threading.Thread(target=consumer, args=(consumer_queues[i], i), daemon=True)
+        consumer_threads.append(t)
+        t.start()
     
+    def feeder():
+        while True:
+            can_reader.send(can.Message(arbitration_id=0x111))
+            time.sleep(1/40)
+
+    t = threading.Thread(target=feeder, daemon=True)
+    t.start()
+    start = datetime.now()
     producer(consumer_queues)
+    can_reader.shutdown()
+    db.f.close()
+    print("run {} seconds, processed {} messages".format((start-datetime.now()), messages))
